e54e362f13cc1cd20461a234ca1a17aa045098c1
George Hu <george_hu@brown.edu>
2018-11-10 17:43:35 -0500

    Added heading

--- weather-request.py
+++ weather-request.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://forecast.weather.gov/MapClick.php?lat=41.823870000000056&lon=-71.41198999999995")
+page = requests.get("https://forecast.weather.gov/MapClick.php?lat=41.82387&lon=-71.4119899")
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -14,6 +14,8 @@ for forecast in detailed_forecast:
     forecasts.append(
         forecast.find(class_ = "forecast-label").get_text()
         + ": " + forecast.find(class_ = "forecast-text").get_text())
-    
+
+print(" 7 Day Forecast ")
+print("----------------") 
 for text_forecast in forecasts:
     print(text_forecast)
